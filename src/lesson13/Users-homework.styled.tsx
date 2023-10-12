892dcfdaa9bd8a8f4ab29e70594c60ddd8a6f0f4
maksym koiev <icmaksym.koiev@americanwell.com>
2023-10-12 03:04:16 +0300

    hw1

--- src/lesson13/Users-homework.styled.tsx
+++ src/lesson13/Users-homework.styled.tsx
@@ -1,5 +1,78 @@
 import styled from "styled-components";
 
+interface IHairColor {
+  hairColor: string;
+}
+
 export const Container = styled.div`
-  display: flex;
+  width: 1120px;
+  margin: 0 auto;
+  padding-top: 169px;
+  //border-bottom: 2px solid grey;
+`;
+
+export const MainContainer = styled.div`
+  background-color: white;
+  color: black;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+`;
+
+export const Header = styled.h2`
+  text-align: left;
+  padding-bottom: 20px;
+  color: var(--Natural-Color, #6f767e);
+  font-family: Roboto;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const TableContainer = styled.table`
+  text-align: left;
+  vertical-align: middle;
+  width: 1120px;
+  height: 52px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: var(--White-Color, #fff);
+  color: var(--Natural-Color, #333);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  font-family: Roboto;
+  border-collapse: collapse;
+`;
+
+export const TableHeader = styled.thead`
+  color: var(--Natural-Color, #6f767e);
+  height: 54px;
+  border-bottom: 19px solid white;
+  vertical-align: middle;
+`;
+
+export const HairColorIcon = styled.p<IHairColor>`
+  background: ${(props) => {
+    switch (props.hairColor) {
+      case "Chestnut":
+        return "#954535";
+        break;
+      case "Blond":
+        return "#F8D96C";
+        break;
+      case "Auburn":
+        return "#964b00";
+        break;
+      default:
+        return props.hairColor;
+        break;
+    }
+  }};
+  border-radius: 100px;
+  width: 21px;
+  height: 21px;
+  flex-shrink: 0;
 `;
--- src/lesson13/Users-homework.tsx
+++ src/lesson13/Users-homework.tsx
@@ -1,9 +1,18 @@
 import usersData from "../users-data";
 import { TUser } from "../users-data";
+import {
+  Container,
+  HairColorIcon,
+  Header,
+  MainContainer,
+  TableContainer,
+  TableHeader,
+} from "./Users-homework.styled";
 
 interface IUserProps {
   data: TUser;
 }
+
 // TODO: Update User component to display user's name, Gender, Hair color, Birth dat and phone number
 // TODO: Style this component using styled-components
 // TODO: Use Users-homework.jpg or Users-homework.fig as a reference
@@ -11,30 +20,135 @@ interface IUserProps {
 // TODO: Add a color prop to HairColorIcon, so it can be used to display different colors
 // TODO: Add your styled-components to src/lesson13/Users-homework.styled.tsx
 
-const User = (props: IUserProps) => {
+const User = (props: IUserProps, id: number) => {
   const { data } = props;
 
   return (
-    <li>
-      {data.firstName} {data.lastName}
-    </li>
+    <tr
+      style={{
+        background: " var(--White-Color, #FFF)",
+        boxShadow: "0px -1px 0px 0px rgba(30, 32, 37, 0.08) inset",
+        width: "1120px",
+        height: "54px",
+        flexShrink: 0,
+      }}
+      key={id}
+    >
+      <td style={{ verticalAlign: "middle", paddingLeft: "91px" }}>
+        {data.firstName} {data.lastName}
+      </td>
+      <td style={{ verticalAlign: "middle", paddingLeft: 91 }}>
+        {CapitaliseWord(data.gender)}
+      </td>
+      <td style={{ verticalAlign: "middle", paddingLeft: 91 }}>
+        <HairColorIcon hairColor={data.hair.color} />
+      </td>
+      <td style={{ verticalAlign: "middle", paddingLeft: 91 }}>
+        <time dateTime="DD-MM-YYY">{FormatData(data.birthDate)}</time>
+      </td>
+      <td style={{ verticalAlign: "middle", paddingLeft: 91 }}>
+        {FormatPhoneNumber(data.phone)}
+      </td>
+    </tr>
+  );
+};
+const TableData = (prop: { data: string; id: number }) => {
+  const { data, id } = prop;
+  return (
+    <td
+      style={{ verticalAlign: "middle", textAlign: "left", paddingLeft: 91 }}
+      key={id}
+    >
+      {data}
+    </td>
+  );
+};
+const TableHead = (props: { data: string[] }) => {
+  const { data } = props;
+  return (
+    <TableHeader>
+      <tr>
+        {data.map((element, index) => (
+          <TableData data={element} id={index} />
+        ))}
+      </tr>
+    </TableHeader>
+  );
+};
+
+const Table = (props: { head: string[] }) => {
+  const { head } = props;
+  return (
+    <TableContainer>
+      {<TableHead data={head} />}
+      <tbody>
+        {usersData.map((user) => (
+          <User data={user} key={user.id} />
+        ))}
+      </tbody>
+    </TableContainer>
   );
 };
 
 export function Users() {
-  // TOOD: update this component to display a header and a list of users
+  const head: string[] = ["Name", "Gender", "Hair Color", "Birthday", "Phone"];
+
+  // TODO: update this component to display a header and a list of users
   // User Name | Gender | Hair Color | Birth date | Phone number
   // TODO: Style this component using styled-components
   // TODO: Use Users-homework.jpg or Users-homework.fig as a reference
   // TODO: Add your styled-components to src/lesson13/Users-homework.styled.tsx
 
   return (
-    <ul>
-      {usersData.map((user) => (
-        <User data={user} key={user.id} />
-      ))}
-    </ul>
+    <MainContainer>
+      <Container>
+        <Header>User</Header>
+        {<Table head={head} />}
+      </Container>
+    </MainContainer>
   );
 }
 
 export default Users;
+
+function FormatData(date: string): string {
+  const oldDate = new Date(date);
+  const month = oldDate.getMonth();
+  const day = oldDate.getDay() + 1;
+  const year = oldDate.getFullYear();
+  return `${day} ${monthDictionary[month]}, ${year}`;
+}
+
+const monthDictionary = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function FormatPhoneNumber(num: string): string {
+  const numArr = num.split("");
+  if (numArr[0] === "+") {
+    numArr.splice(0, 1);
+  }
+  numArr.forEach((char, index) => {
+    if (char === " ") {
+      numArr.splice(index, 1);
+    }
+  });
+  return numArr.join("");
+}
+
+function CapitaliseWord(word: string): string {
+  const toCpt = word.split("");
+  toCpt[0] = toCpt[0].toUpperCase();
+  return toCpt.join("");
+}
